0ca1957f61e0dd05eb75a626aba525048393c722
Kris Chery <kchery@ebay.com>
2026-02-09 15:08:19 -0600

    Refactored add-to-cart functionality to use FormData for improved handling of item properties and enhanced error management. Updated button label for clarity and added asynchronous handling for cart updates and drawer rendering.

--- assets/main.js
+++ assets/main.js
@@ -30355,51 +30355,106 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _regenerator() { /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */ var e, t, r = "function" == typeof Symbol ? Symbol : {}, n = r.iterator || "@@iterator", o = r.toStringTag || "@@toStringTag"; function i(r, n, o, i) { var c = n && n.prototype instanceof Generator ? n : Generator, u = Object.create(c.prototype); return _regeneratorDefine2(u, "_invoke", function (r, n, o) { var i, c, u, f = 0, p = o || [], y = !1, G = { p: 0, n: 0, v: e, a: d, f: d.bind(e, 4), d: function d(t, r) { return i = t, c = 0, u = e, G.n = r, a; } }; function d(r, n) { for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) { var o, i = p[t], d = G.p, l = i[2]; r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0)); } if (o || r > 1) return a; throw y = !0, n; } return function (o, p, l) { if (f > 1) throw TypeError("Generator is already running"); for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) { i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u); try { if (f = 2, i) { if (c || (o = "next"), t = i[o]) { if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object"); if (!t.done) return t; u = t.value, c < 2 && (c = 0); } else 1 === c && (t = i["return"]) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1); i = e; } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break; } catch (t) { i = e, c = 1, u = t; } finally { f = 1; } } return { value: t, done: y }; }; }(r, o, i), !0), u; } var a = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} t = Object.getPrototypeOf; var c = [][n] ? t(t([][n]())) : (_regeneratorDefine2(t = {}, n, function () { return this; }), t), u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c); function f(e) { return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine2(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e; } return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine2(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine2(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine2(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine2(u), _regeneratorDefine2(u, o, "Generator"), _regeneratorDefine2(u, n, function () { return this; }), _regeneratorDefine2(u, "toString", function () { return "[object Generator]"; }), (_regenerator = function _regenerator() { return { w: i, m: f }; })(); }
+function _regeneratorDefine2(e, r, n, t) { var i = Object.defineProperty; try { i({}, "", {}); } catch (e) { i = 0; } _regeneratorDefine2 = function _regeneratorDefine(e, r, n, t) { function o(r, n) { _regeneratorDefine2(e, r, function (e) { return this._invoke(r, n, e); }); } r ? i ? i(e, r, { value: n, enumerable: !t, configurable: !t, writable: !t }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2)); }, _regeneratorDefine2(e, r, n, t); }
+function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
+function _asyncToGenerator(n) { return function () { var t = this, e = arguments; return new Promise(function (r, o) { var a = n.apply(t, e); function _next(n) { asyncGeneratorStep(a, r, o, _next, _throw, "next", n); } function _throw(n) { asyncGeneratorStep(a, r, o, _next, _throw, "throw", n); } _next(void 0); }); }; }
 
 var CustomAddToCartButtonSifter = function CustomAddToCartButtonSifter() {
+  var _container$dataset, _container$dataset2, _container$dataset3;
   var container = document.getElementById('product-form-buttons-holder-react');
-  var variantId = container.dataset.variantId;
-  var subscriptionVariantId = container.dataset.subscriptionVariantId;
-  var sellingPlanId = container.dataset.sellingPlanId;
+  var variantId = container === null || container === void 0 || (_container$dataset = container.dataset) === null || _container$dataset === void 0 ? void 0 : _container$dataset.variantId;
+  var subscriptionVariantId = container === null || container === void 0 || (_container$dataset2 = container.dataset) === null || _container$dataset2 === void 0 ? void 0 : _container$dataset2.subscriptionVariantId;
+  var sellingPlanId = container === null || container === void 0 || (_container$dataset3 = container.dataset) === null || _container$dataset3 === void 0 ? void 0 : _container$dataset3.sellingPlanId;
   var generateSerialNumber = function generateSerialNumber() {
     var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var twoLetters = letters[Math.floor(Math.random() * 26)] + letters[Math.floor(Math.random() * 26)];
     var fiveNumbers = String(Math.floor(10000 + Math.random() * 90000));
     return twoLetters + '-' + fiveNumbers;
   };
-  var addToCart = function addToCart() {
-    var webSerialNumber = generateSerialNumber();
-    var items = [{
-      id: variantId,
-      quantity: 1,
-      properties: {
-        webSerialNumber: webSerialNumber
-      }
-    }, {
-      id: subscriptionVariantId,
-      quantity: 1,
-      selling_plan: sellingPlanId,
-      properties: {
-        webSerialNumber: webSerialNumber
-      }
-    }];
-    fetch("".concat(window.routes.cart_add_url, ".js"), {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        items: items
-      })
-    }).then(function (response) {
-      return response.json();
-    }).then(function (data) {
-      if (data.status) console.error(data.errors);
-    })["catch"](console.error);
-  };
+  var addToCart = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee() {
+      var webSerialNumber, formData, response, data, sectionsResponse, sectionsHTML, cartDrawer, parser, doc, newDrawerInner, currentDrawerInner, _t;
+      return _regenerator().w(function (_context) {
+        while (1) switch (_context.p = _context.n) {
+          case 0:
+            webSerialNumber = generateSerialNumber();
+            formData = new FormData();
+            formData.append('items[0][id]', variantId);
+            formData.append('items[0][quantity]', '1');
+            formData.append('items[0][properties][Web-Serial-Number]', webSerialNumber);
+            formData.append('items[1][id]', subscriptionVariantId);
+            formData.append('items[1][quantity]', '1');
+            formData.append('items[1][selling_plan]', sellingPlanId);
+            formData.append('items[1][properties][Web-Serial-Number]', webSerialNumber);
+            _context.p = 1;
+            _context.n = 2;
+            return fetch(window.routes.cart_add_url, {
+              method: 'POST',
+              headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Accept': 'application/javascript'
+              },
+              body: formData
+            });
+          case 2:
+            response = _context.v;
+            _context.n = 3;
+            return response.json();
+          case 3:
+            data = _context.v;
+            if (!data.status) {
+              _context.n = 4;
+              break;
+            }
+            console.error(data.description);
+            return _context.a(2);
+          case 4:
+            _context.n = 5;
+            return fetch("".concat(window.routes.cart_url, "?section_id=cart-drawer"));
+          case 5:
+            sectionsResponse = _context.v;
+            _context.n = 6;
+            return sectionsResponse.text();
+          case 6:
+            sectionsHTML = _context.v;
+            // Step 3: Render and open drawer
+            cartDrawer = document.querySelector('cart-drawer');
+            if (cartDrawer) {
+              parser = new DOMParser();
+              doc = parser.parseFromString(sectionsHTML, 'text/html');
+              newDrawerInner = doc.querySelector('.drawer__inner');
+              if (newDrawerInner) {
+                currentDrawerInner = cartDrawer.querySelector('.drawer__inner');
+                if (currentDrawerInner) {
+                  currentDrawerInner.innerHTML = newDrawerInner.innerHTML;
+                }
+              }
+
+              // Remove empty class if present
+              cartDrawer.classList.remove('is-empty');
+              if (typeof cartDrawer.open === 'function') {
+                cartDrawer.open();
+              }
+            }
+            _context.n = 8;
+            break;
+          case 7:
+            _context.p = 7;
+            _t = _context.v;
+            console.error('Add to cart error:', _t);
+          case 8:
+            return _context.a(2);
+        }
+      }, _callee, null, [[1, 7]]);
+    }));
+    return function addToCart() {
+      return _ref.apply(this, arguments);
+    };
+  }();
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "Checkbox 1"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "Checkbox 2"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", {
     onClick: addToCart
-  }, "Add to Cart 23 1023"));
+  }, "Add to Cart"));
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CustomAddToCartButtonSifter);
 
--- src/js/customAddToCartButtonSifter.jsx
+++ src/js/customAddToCartButtonSifter.jsx
@@ -3,39 +3,77 @@ import React from 'react';
 const CustomAddToCartButtonSifter = () => {
 
   const container = document.getElementById('product-form-buttons-holder-react');
-  const variantId = container.dataset.variantId;
-  const subscriptionVariantId = container.dataset.subscriptionVariantId;
-  const sellingPlanId = container.dataset.sellingPlanId;
-
+  const variantId = container?.dataset?.variantId;
+  const subscriptionVariantId = container?.dataset?.subscriptionVariantId;
+  const sellingPlanId = container?.dataset?.sellingPlanId;
 
   const generateSerialNumber = () => {
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const twoLetters = letters[Math.floor(Math.random() * 26)] + letters[Math.floor(Math.random() * 26)];
     const fiveNumbers = String(Math.floor(10000 + Math.random() * 90000));
     return twoLetters + '-' + fiveNumbers;
-  }
+  };
 
-  const addToCart = () => {
-
+  const addToCart = async () => {
     const webSerialNumber = generateSerialNumber();
-
-    const items = [
-        { id: variantId, quantity: 1, properties: { webSerialNumber } },
-        { id: subscriptionVariantId, quantity: 1, selling_plan: sellingPlanId, properties: { webSerialNumber } }
-    ]
-
-    fetch(`${window.routes.cart_add_url}.js`, {
+    const formData = new FormData();
+    
+    formData.append('items[0][id]', variantId);
+    formData.append('items[0][quantity]', '1');
+    formData.append('items[0][properties][Web-Serial-Number]', webSerialNumber);
+    
+    formData.append('items[1][id]', subscriptionVariantId);
+    formData.append('items[1][quantity]', '1');
+    formData.append('items[1][selling_plan]', sellingPlanId);
+    formData.append('items[1][properties][Web-Serial-Number]', webSerialNumber);
+    
+    try {
+      // Step 1: Add to cart
+      const response = await fetch(window.routes.cart_add_url, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({items})
-    }).then(response => response.json())
-    .then((data) => {
-        if(data.status) console.error(data.errors)
-    })
-    .catch(console.error)
-
-
-  } 
+        headers: { 
+          'X-Requested-With': 'XMLHttpRequest',
+          'Accept': 'application/javascript'
+        },
+        body: formData
+      });
+      
+      const data = await response.json();
+      
+      if (data.status) {
+        console.error(data.description);
+        return;
+      }
+      
+      // Step 2: Fetch cart sections separately
+      const sectionsResponse = await fetch(`${window.routes.cart_url}?section_id=cart-drawer`);
+      const sectionsHTML = await sectionsResponse.text();
+      
+      // Step 3: Render and open drawer
+      const cartDrawer = document.querySelector('cart-drawer');
+      if (cartDrawer) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(sectionsHTML, 'text/html');
+        const newDrawerInner = doc.querySelector('.drawer__inner');
+        
+        if (newDrawerInner) {
+          const currentDrawerInner = cartDrawer.querySelector('.drawer__inner');
+          if (currentDrawerInner) {
+            currentDrawerInner.innerHTML = newDrawerInner.innerHTML;
+          }
+        }
+        
+        // Remove empty class if present
+        cartDrawer.classList.remove('is-empty');
+        
+        if (typeof cartDrawer.open === 'function') {
+          cartDrawer.open();
+        }
+      }
+    } catch (error) {
+      console.error('Add to cart error:', error);
+    }
+  };
 
   return (
     <div>
@@ -45,7 +83,7 @@ const CustomAddToCartButtonSifter = () => {
       <div>
         Checkbox 2
       </div>
-      <button onClick={addToCart}>Add to Cart 23 1023</button>
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
   );
 };
